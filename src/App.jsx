3d1d2a7726b3c26cc2b4ed7ac86214d05a7e84bc
amarnathbhat <bhatamar8@gmail.com>
2024-07-01 18:26:10 +0530

    Add: Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
-    <div className="flex flex-col p-10 m-10 justify-between items-center bg-red-400">
-      <h1 className="text-4xl font-bold">Hello, World!</h1>
-      <h2 className="text-2xl font-bold">New Website</h2>
+    <div>
+      <Navbar />
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+
+const NAV_ITEMS = [
+  {
+    label: "Men",
+    href: "/men",
+  },
+  {
+    label: "Women",
+    href: "/women",
+  },
+  {
+    label: "Kids",
+    href: "/kids",
+  },
+  {
+    label: "Beauty",
+    href: "/beauty",
+  },
+  {
+    label: "Home & Kitchen",
+    href: "/home",
+  },
+];
+
+const EXTRA_ITEMS = [
+  {
+    label: "Login",
+    href: "/login",
+  },
+  {
+    label: "Sign Up",
+    href: "/signup",
+  },
+];
 
 const Navbar = () => {
+  const [mobile, setMobile] = useState(false);
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flex flex-col justify-center py-5 px-10 text-xl items-center navy-blue text-[ivory]">
+      <img src="/logo.png" alt="Meesho" className="w-[10%] h-[5%] p-5" />
+      {mobile === false && (
+        <div className="flex w-full justify-around p-5">
+          <div className="flex justify-between gap-20">
+            {NAV_ITEMS.map((item, index) => (
+              <a key={index} href={item.href}>
+                {item.label}
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
