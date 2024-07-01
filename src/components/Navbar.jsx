0f726041ffe88aecf46ed7a37911b18338f309f5
chronus091 <gautambusiness777@gmail.com>
2024-07-01 14:31:45 +0530

    Update: Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,27 +24,63 @@ const NAV_ITEMS = [
   },
 ];
 
-const EXTRA_ITEMS = [
+
+const first = [
   {
     label: "Login",
     href: "/login",
   },
   {
-    label: "Sign Up",
+    label: "Sign-up",
     href: "/signup",
   },
 ];
-
+const basic = [
+  {
+    label: "About",
+    href: "/about",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+];
+const second = [
+  {
+    label: "Profile",
+    href: "/login",
+  },
+  {
+    label: "Cart",
+    href: "/signup",
+  },
+];
 const Navbar = () => {
+  const [login, islogin]=useState(false)
   const [mobile, setMobile] = useState(false);
   return (
-    <nav className="flex flex-col justify-center py-5 px-10 text-xl items-center navy-blue text-[ivory]">
-      <img src="/logo.png" alt="Meesho" className="w-[10%] h-[5%] p-5" />
+    <nav className="flex flex-col justify-center text-xl items-center navy-blue text-[ivory]">
+      <div className="flex justify-between w-full fontfam m-0 p-0">
+        <div className="flex pl-3">{basic.map((item, index) => (
+                <a className=" hover:scale-105 p-3 hover:bg-gradient-to-b h-[60px] hover:from-[orange] w-[100px] flex justify-center" key={index} href={item.href}>
+                {item.label}
+              </a>
+            ))}</div><img src="/logo.png" alt="Meesho" className="h-[20%] w-[20%] p-5 hover:scale-105"/>
+        <div className="flex pr-3">{login && first.map((item, index) => (
+              <a className=" hover:scale-105 p-3 hover:bg-gradient-to-b  h-[60px] hover:from-[orange]  w-[100px] flex justify-center" key={index} href={item.href}>
+                {item.label}
+              </a>
+            ))}{!login && second.map((item, index) => (
+              <a  className=" hover:scale-105 p-3 hover:bg-gradient-to-b h-[60px] hover:from-[orange]  w-[100px] flex justify-center" key={index} href={item.href}>
+                {item.label}
+              </a>
+            ))}</div>
+      </div>
       {mobile === false && (
         <div className="flex w-full justify-around p-5">
-          <div className="flex justify-between gap-20">
+          <div className="flex justify-between gap-20 fontfam">
             {NAV_ITEMS.map((item, index) => (
-              <a key={index} href={item.href}>
+              <a className=" hover:scale-105 hover:text-[orange]" key={index} href={item.href}>
                 {item.label}
               </a>
             ))}
