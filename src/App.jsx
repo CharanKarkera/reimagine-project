c15b236fb9cd4fd4392c23a10d5d063c0756f5f6
mayurjathan <mayurjathan999@gmail.com>
2024-07-03 11:26:53 +0530

    Add : Splashscreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
-import React from "react";
+import React , { useState} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Components
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Featured from "./components/Featured";
-
+import SplashScreen from "./components/Splashscreen";
 // Pages
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 
 const App = () => {
+  const [isVideoEnded, setIsVideoEnded] = useState(false);
+
+  const handleVideoEnd = () => {
+    setIsVideoEnded(true);
+  };
+
   return (
     <Router>
-      <div>
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Featured />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      {!isVideoEnded && <SplashScreen onVideoEnd={handleVideoEnd} />}
+      {isVideoEnded && (
+       <div>
+       <Navbar />
+       <main>
+         <Routes>
+           <Route path="/" element={<Featured />} />
+           <Route path="/home" element={<Home />} />
+           <Route path="/login" element={<Login />} />
+           <Route path="/signup" element={<Signup />} />
+         </Routes>
+       </main>
+       <Footer />
+     </div>
+      )}
     </Router>
   );
 };
