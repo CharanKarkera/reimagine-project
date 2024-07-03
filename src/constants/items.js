b7f7ff57d4444dc72bd47eeef297390f0bd41ea0
mayurjathan <mayurjathan999@gmail.com>
2024-07-03 13:44:35 +0530

    Add : images & icons

---
+++ src/constants/items.js
@@ -0,0 +1,178 @@
+export const men = [
+    {
+        img:"/public/clothes/1718007632_2135871.webp",
+        name:"Plaid: Chestnut",
+        desc:"Men Textured Shirts",
+        price:"1199",
+        org:"1799",
+        dis:"30%",
+        rate:"4.5",
+        href:"/"
+    },
+    {
+        img:"/public/clothes/1718348893_6177751.webp",
+        name:"Marvel: Creative Chaos",
+        desc:"Oversized T-Shirts",
+        price:"799",
+        org:"1099",
+        dis:"30%",
+        rate:"4.2",
+        href:"/"
+    },
+    {
+        img:"/public/clothes/1718360900_9529243.webp",
+        name:"Daffy Duck: Prankster",
+        desc:"Oversized T-Shirts",
+        price:"699",
+        org:"999",
+        dis:"30%",
+        rate:"4.4",
+        href:"/"
+    },
+    {
+        img:"/public/clothes/1718456679_3238063.webp",
+        name:"Solids: Burgundy",
+        desc:"Men Textured Shirts",
+        price:"1199",
+        org:"1499",
+        dis:"20%",
+        rate:"4.1",
+        href:"/"
+    },
+    {
+        img:"/public/clothes/1718951010_9202963.webp",
+        name:"TSS Originals: Break Free Turtle",
+        desc:"Oversized T-Shirts",
+        price:"599",
+        org:"999",
+        dis:"40%",
+        rate:"4.8",
+        href:"/"
+    },
+    {
+        img:"/public/clothes/1719657082_6280490.webp",
+        name:"Textured Shirt: Magnum",
+        desc:"Men Textured Shirts",
+        price:"999",
+        org:"1499",
+        dis:"25%",
+        rate:"3.7",
+        href:"/"
+    },
+    {
+        img:"/public/clothes/1719676176_6347310.webp",
+        name:"TSS Originals: Spirit Mosaic",
+        desc:"Oversized T-Shirts",
+        price:"899",
+        org:"1399",
+        dis:"35%",
+        rate:"4.7",
+        href:"/"
+    },
+    {
+        img:"/public/clothes/1719838192_2380346.webp",
+        name:"Solids: Celestial Green",
+        desc:"Shirts",
+        price:"1099",
+        org:"1699",
+        dis:"30%",
+        rate:"4.1",
+        href:"/"
+    },
+    {
+        img:"/public/clothes/9teli_400.webp",
+        name:"Mufti Men's Solid Slim Spread Collar Navy Blue Shirts",
+        desc:"Shirts",
+        price:"2499",
+        org:"3199",
+        dis:"15%",
+        rate:"4.2",
+        href:"/"
+    },
+  ];
+  
+ export const women = [
+    {
+        img:"/public/clothes/0wkbh_400.webp",
+        name:"Ishin Women's Dresses",
+        desc:"Fabric : Viscose Rayon ",
+        price:"2660",
+        org:"2799",
+        dis:"5%",
+        rate:"4.3",
+        href:"/"
+    },
+    {
+        img:"/public/clothes/8jhgk_400.webp",
+        name:"Ishin Women's Ishin Dresses",
+        desc:"Fabric : Cotton ",
+        price:"4999",
+        org:"4999",
+        dis:"0%",
+        rate:"4.1",
+        href:"/"
+    },
+    {
+        img: "/public/clothes/sctqe_512.webp",
+        name: "Zara Women's Floral Dress",
+        desc: "Fabric: Polyester",
+        price: "3499",
+        org: "3999",
+        dis: "13%",
+        rate: "4.6",
+        href: "/"
+    },
+    {
+        img: "/public/clothes/hgtkp_400.webp",
+        name: "H&M Women's Summer Dress",
+        desc: "Fabric: Linen",
+        price: "2999",
+        org: "3299",
+        dis: "9%",
+        rate: "4.5",
+        href: "/"
+    }
+ ];  
+ export const kids = [
+    {
+        img:"/public/clothes/0wkbh_400.webp",
+        name:"Pink Drop Waist Tank Dress",
+        desc:"Fabric : Cotton ",
+        price:"1,994",
+        org:"1,994",
+        dis:"0%",
+        rate:"4.1",
+        href:"/"
+    },
+    {
+        img: "/public/clothes/t85is_400.webp",
+        name: "Pink Floral Midi Dress",
+        desc: "Fabric: Polyester",
+        price: "2,299",
+        org: "2,499",
+        dis: "8%",
+        rate: "4.4",
+        href: "/"
+    },
+    {
+        img: "/public/clothes/lggir_400.webp",
+        name: "Pink A-line Dress",
+        desc: "Fabric: Viscose Rayon",
+        price: "2,799",
+        org: "2,999",
+        dis: "7%",
+        rate: "4.5",
+        href: "/"
+    },
+    {
+        img: "/public/clothes/knhru_400.webp",
+        name: "Orange Evening Gown",
+        desc: "Fabric: Silk",
+        price: "5,499",
+        org: "5,999",
+        dis: "8%",
+        rate: "4.7",
+        href: "/"
+    }
+ ];
+  
